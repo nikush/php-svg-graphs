50ee4719721a9262e28b65869a3d1b47cbee700d
Nikush Patel <nikush.patel7@gmail.com>
2012-09-12 13:18:16 +0100

    Improve path drawing with path class.

--- svg.php
+++ svg.php
@@ -112,15 +112,14 @@ class Svg
     /**
      * Add a path to the image.
      *
-     * @param   string  $commands
      * @param   array   $attrs
-     * @return  void
+     * @return  SvgPath
      */
-    public function addPath($commands, $attrs=array())
+    public function addPath($attrs=array())
     {
         $path = $this->root_node->addChild('path');
-        $attrs['d'] = $commands;
         $this->setAttributes($path, $attrs);
+        return new SvgPath($path);
     }
 
     /**
@@ -280,5 +279,119 @@ class SvgPolyline
         $new_point[1] += $y;
         $this->last_point = $new_point;
         $this->polyline['points'] .= "{$new_point[0]},{$new_point[1]} ";
+    }
+}
+
+/**
+ * SVG path element.
+ *
+ * @author  Nikush Patel
+ */
+class SvgPath
+{
+    /**
+     * The path element.
+     *
+     * @var SimpleXMLElement
+     */
+    private $path;
+
+    /**
+     * Wrap the class around a blank path element to decorate it with path
+     * drawing methods.
+     *
+     * @param   SimpleXMLElement    $path
+     */
+    public function __construct(SimpleXMLElement $path)
+    {
+        $this->path = $path;
+        $this->path['d'] = '';
+    }
+
+    /**
+     * Move the pen to the point.
+     *
+     * @param   number  $x
+     * @param   number  $y
+     * @param   boolean $relative
+     * @return  void
+     */
+    public function moveTo($x, $y, $relative=true)
+    {
+        $c = $relative ? 'm' : 'M';
+        $this->path['d'] .= "$c$x,$y ";
+    }
+
+    /**
+     * Draw a line to the point.
+     *
+     * @param   number  $x
+     * @param   number  $y
+     * @param   boolean $relative
+     * @return  void
+     */
+    public function lineTo($x, $y, $relative=true)
+    {
+        $c = $relative ? 'l' : 'L';
+        $this->path['d'] .= "$c$x,$y ";
+    }
+
+    /**
+     * Draw a horizontal line the x coordinate.
+     *
+     * @param   number  $x
+     * @param   number  $y
+     * @param   boolean $relative
+     * @return  void
+     */
+    public function horizontalLineTo($x, $relative=true)
+    {
+        $c = $relative ? 'h' : 'H';
+        $this->path['d'] .= "$c$x ";
+    }
+
+    /**
+     * Draw a vertical line to the y coordinate.
+     *
+     * @param   number  $x
+     * @param   number  $y
+     * @param   boolean $relative
+     * @return  void
+     */
+    public function verticalLineTo($y, $relative=true)
+    {
+        $c = $relative ? 'v' : 'V';
+        $this->path['d'] .= "$c$y ";
+    }
+
+    /**
+     * Draw an eliptical arch.
+     *
+     * @param   number  $rx x-axis radius
+     * @param   number  $ry y-axis radius
+     * @param   number  $xr x-axis rotation
+     * @param   boolean $large  Take the long way round
+     * @param   boolean $sweep  Draw arch inside or outside
+     * @param   number  $x  end point
+     * @param   number  $y  end point
+     * @param   boolean $relative
+     * @return  void
+     */
+    public function archTo($rx, $ry, $xr, $large, $sweep, $x, $y, $relative=true)
+    {
+        $c = $relative ? 'a' : 'A';
+        $l = $large ? 1 : 0;
+        $s = $sweep ? 1 : 0;
+        $this->path['d'] .= "$c$rx,$ry $xr $l,$s $x,$y ";
+    }
+
+    /**
+     * Close the path.
+     *
+     * @return  void
+     */
+    public function closePath()
+    {
+        $this->path['d'] .= 'z';
     }
 }
